5d74108a44356be3a65a9119490bbecf2a495046
erika-xicale <erikaxicale@gmail.com>
2024-08-13 23:03:11 -0600

    Primeros cambios karla

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,19 +1,15 @@
 import DesktopNavbar from './reposive/desktop';
-import MobileNavbar from './reposive/mobile';
+/*import MobileNavbar from './reposive/mobile';
 import TabletNavbar from './reposive/tablet';
-
+*/
 export default async function Navbar() {
   return (
     <>
       <div className="hidden lg:block">
         <DesktopNavbar />
       </div>
-      <div className="md:hidden">
-        <MobileNavbar />
-      </div>
-      <div className="hidden md:block lg:hidden">
-        <TabletNavbar />
-      </div>
+
+
     </>
   );
 }
--- components/layout/navbar/reposive/desktop.tsx
+++ components/layout/navbar/reposive/desktop.tsx
@@ -1,28 +1,13 @@
-import Cart from 'components/cart';
-import Menu from 'components/layout/navigation';
-import User from 'components/user';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Suspense } from 'react';
-import Search from '../search';
 
 export default function DesktopNavbar() {
   return (
     <>
-      <nav className="bg-[#f7e7da]">
+      <nav className="bg-[#ffffff]">
+
         <div className="grid grid-cols-5 content-center items-center pb-3 uppercase">
-          <div className="col-span-2 block">
-            <div className="flex w-full flex-row justify-end gap-x-10">
-              <div className="text-black underline-offset-4 hover:text-neutral-500 hover:underline dark:text-black dark:hover:text-neutral-300">
-                Nosotros
-              </div>
-              <div className="relative">
-                <Suspense>
-                  <Menu />
-                </Suspense>
-              </div>
-            </div>
-          </div>
           <div>
             <Link
               href="/"
@@ -32,7 +17,7 @@ export default function DesktopNavbar() {
               <div className="relative h-[100px] w-full">
                 <Image
                   className="object-contain"
-                  src={'/logoNegro.png'}
+                  src={'/Logo Karl Karla_Escritorio.png'}
                   alt=""
                   fill
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -40,28 +25,39 @@ export default function DesktopNavbar() {
               </div>
             </Link>
           </div>
+          <div className="grid grid-cols-5 content-center items-center pb-3 uppercase">
+            <Link
+              href="/"
+              aria-label="Go back home"
+              className="mr-2 flex w-auto items-center justify-center lg:mr-6"
+            >
+              <div className="relative h-[100px] w-full">
+                <Image
+                  className="object-contain"
+                  src={'/Menu.png'}
+                  alt=""
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+              </div>
+            </Link>
+
+
+
+          </div>
+
+
           <div className="md:col-span-2">
             <div className="flex flex-row items-center space-x-3">
-              <div className="w-1/3">
-                <Suspense>
-                  <Search />
-                </Suspense>
-              </div>
-              <div className="hover:cursor-pointer">
-                <Suspense>
-                  <User />
-                </Suspense>
-              </div>
-              <div className="hover:cursor-pointer">
-                <Cart />
-              </div>
+
+
               <div className="flex w-2/4 justify-end space-x-3 pr-10">
                 <Link href="">
                   <div className="relative h-[25px] w-[25px]">
                     <Suspense>
                       <Image
                         className="object-cover"
-                        src={'/facebookRosa.png'}
+                        src={'/Buscar.png'}
                         alt=""
                         fill
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -74,7 +70,7 @@ export default function DesktopNavbar() {
                     <Suspense>
                       <Image
                         className="object-cover"
-                        src={'/instagramRosa.png'}
+                        src={'/Perfil.png'}
                         alt=""
                         fill
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -82,6 +78,20 @@ export default function DesktopNavbar() {
                     </Suspense>
                   </div>
                 </Link>
+                <Link href="">
+                  <div className="relative h-[25px] w-[25px]">
+                    <Suspense>
+                      <Image
+                        className="object-cover"
+                        src={'/Carrito.png'}
+                        alt=""
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      />
+                    </Suspense>
+                  </div>
+                </Link>
+
               </div>
             </div>
           </div>
